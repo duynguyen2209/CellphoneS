0358b2c1c5c1ed5e9d6c056d0cf74154248cb5da
Nguyen Duc DUy <72897050+duynguyen2209@users.noreply.github.com>
2021-11-25 13:37:01 +0700

    Up

---
+++ cps.js
@@ -0,0 +1,36 @@
+// Slider
+window.onscroll = function() {myFunction();scrollFunction();};
+// Get the navbar
+var headerNav = document.getElementById("navbar");
+// Get the offset position of the navbar
+var sticky = headerNav.offsetTop;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      headerNav.classList.add("sticky")
+    } else {
+      headerNav.classList.remove("sticky");
+    }
+  }
+
+// Button back to top
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
