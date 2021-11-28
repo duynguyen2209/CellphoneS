97376745297b28fe9155de51f4481bcebf097673
Nguyen Duc DUy <72897050+duynguyen2209@users.noreply.github.com>
2021-11-29 01:19:44 +0700

    119

--- product-info/prd.js
+++ product-info/prd.js
@@ -22,9 +22,9 @@ let thumbnails = document.getElementsByClassName('thumbnail')
     let buttonLeft = document.getElementById('slideLeft');
 
     buttonLeft.addEventListener('click', function(){
-        document.getElementById('slider').scrollLeft -= 180
+        document.getElementById('slider').scrollLeft -= 220
     })
 
     buttonRight.addEventListener('click', function(){
-        document.getElementById('slider').scrollLeft += 180
+        document.getElementById('slider').scrollLeft += 220
     })
