5df41b9aa944fd1311ffc446be192e85ed563683
Nguyen Duc DUy <72897050+duynguyen2209@users.noreply.github.com>
2021-11-30 14:10:26 +0700

    js

--- product-info/prd.js
+++ product-info/prd.js
@@ -28,3 +28,35 @@ let thumbnails = document.getElementsByClassName('thumbnail')
     buttonRight.addEventListener('click', function(){
         document.getElementById('slider').scrollLeft += 220
     })
+
+
+// Open modal info
+var modalInfo = document.querySelector('.modal-tech');
+var btnOpen = document.querySelector('.btn-show-more');
+var btnClose = document.querySelector('.btn-close-modal');
+var btnClose2 = document.querySelector('.close-item');
+
+
+btnOpen.onclick = function(){
+    modalInfo.style.display = "block";
+    document.querySelector("body").style.overflow = 'hidden';
+}
+
+btnClose.onclick = function(){
+    modalInfo.style.display = "none";
+    document.querySelector("body").style.overflow = 'visible';
+
+}
+
+btnClose2.onclick = function(){
+    modalInfo.style.display = "none";
+    document.querySelector("body").style.overflow = 'visible';
+
+}
+
+window.onclick = function(event) {
+    if (event.target == modalInfo) {
+    modalInfo.style.display = "none";
+    document.querySelector("body").style.overflow = 'visible';
+    }
+  }
